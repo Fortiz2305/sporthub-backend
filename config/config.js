17821decc06558f7ef054ab155854b076a6543e0
Francisco Ortiz <fortizabril@gmail.com>
2017-07-15 20:03:43 +0200

    Update db config

--- config/config.js
+++ config/config.js
@@ -5,9 +5,7 @@ module.exports = {
   database: {
     host: process.env.DATABASE_HOST || 'localhost',
     port: process.env.DATABASE_PORT || 27017,
-    name: process.env.DATABASE_NAME || 'sporthub',
-    user: process.env.DATABASE_USER || 'user',
-    password: process.env.DATABASE_PASSWORD || 'password'
+    name: process.env.DATABASE_NAME || 'SportHub',
   },
   jwtSecret: 'veryverysecretphrase'
 };
