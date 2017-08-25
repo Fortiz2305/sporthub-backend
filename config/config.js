34f7733ee3bfa5538861f65c3f8ebef27c607e1c
Francisco Ortiz <fortizabril@gmail.com>
2017-08-25 17:37:08 +0200

    Add gcloud info

--- config/config.js
+++ config/config.js
@@ -6,6 +6,8 @@ module.exports = {
     host: process.env.DATABASE_HOST || 'localhost',
     port: process.env.DATABASE_PORT || 27017,
     name: process.env.DATABASE_NAME || 'SportHub',
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD
   },
   jwtSecret: 'veryverysecretphrase'
 };
